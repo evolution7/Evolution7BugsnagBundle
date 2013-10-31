84c1f5d8b1928b2f6c91d924f4e3cc1041debd9f
Arjen Schwarz <arjen@evolution7.com.au>
2013-10-31 12:07:45 +1100

    Documentation for the ReleaseStageInterface

--- ReleaseStage/ReleaseStageInterface.php
+++ ReleaseStage/ReleaseStageInterface.php
@@ -1,7 +1,18 @@
 <?php
 namespace Evolution7\BugsnagBundle\ReleaseStage;
 
+/**
+ * Interface for ReleaseStage classes
+ *
+ * These classes are used to determine which release stage the application
+ * is deployed in
+ */
 interface ReleaseStageInterface
 {
+    /**
+     * Returns a textual description of the release stage
+     *
+     * @return string a textual description of the release stage
+     */
     public function get();
 }
